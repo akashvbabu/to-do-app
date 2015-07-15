826903914e4629075b2e70d78108f2c65714260d
Babu <ababu@hqababu902.local>
2015-07-15 13:41:23 -0400

    Modified index.js to reflect add/delete actions for tasks

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
+var fs = require('fs');
+var tasks = require('../public/to_do_list.json');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('./to-do.html');
@@ -8,12 +11,33 @@ router.get('/', function(req, res, next) {
 
 router.get('/remove_action',function(req,res){
   var task_to_be_removed = req.param('task_name');
-  console.log(task_to_be_removed);
+  var index_to_be_removed = _.findIndex(tasks,'name',task_to_be_removed);
+  tasks.splice(index_to_be_removed);
+  fs.writeFile('./public/to_do_list.json',JSON.stringify(tasks,null,4),function(err){
+    console.log(err);
+  })
+  res.redirect('/');
 });
 
 router.get('/newtask',function(req,res){
   console.log('inside adding new task');
   //redirect to new html page to add new task
+  res.render('add-task.html');
+});
+
+router.post('/addtask',function(req,res){
+  var task_to_be_added = req.body.task_name_field;
+  var task_desc = req.body.task_description_field;
+  var obj = {name:task_to_be_added,description:task_desc};
+  if(_.findIndex(tasks,obj)==-1){
+    tasks.push(obj);
+    console.log(tasks);
+    fs.writeFile('./public/to_do_list.json',JSON.stringify(tasks,null,4),function(err){
+      console.log(err);
+    })
+  }else {
+    console.log('already present');
+  }
   res.redirect('/');
 });
 module.exports = router;
